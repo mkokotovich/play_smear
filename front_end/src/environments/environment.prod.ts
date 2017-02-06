22f282430f95c942eb1e46b0d14bdf4873637baa
Matthew Kokotovich <mkokotovich@gmail.com>
2017-02-06 15:00:40 -0600

    fix typo

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true
-  apiBaseUrl: "https://playsmear.herokuapps.com"
+  production: true;
+  apiBaseUrl: "https://playsmear.herokuapps.com";
 };
--- front_end/src/environments/environment.ts
+++ front_end/src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
-  apiBaseUrl: "http://localhost:5000"
+  production: false;
+  apiBaseUrl: "http://localhost:5000";
 };
