4a4856837c11745479bcb006710897d7c07186c2
Matthew Kokotovich <mkokotovich@gmail.com>
2017-02-07 10:43:55 -0600

    fix conf

--- play_smear_api/tools/test_json_post.sh
+++ play_smear_api/tools/test_json_post.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-#curl -i -H "Content-Type: application/json" -X POST -d '{"numPlayers":"1", "username": "matt"}' http://localhost:5000/api/startgame/
-curl -s -H "Content-Type: application/json" -X POST -d '{"numPlayers":"3", "username": "matt"}' http://localhost:5000/api/startgame/ | python -m json.tool
+set -xeuo pipefail
+HOST="localhost:5000"
+if [ $# -eq 1 ] ; then
+    HOST=$1
+fi
+URL="http://${HOST}"
+#curl -i -H "Content-Type: application/json" -X POST -d '{"numPlayers":"1"}' "${URL}/api/game/create/"
+curl -s -H "Content-Type: application/json" -X POST -d '{"numPlayers":"3"}' "${URL}/api/game/create/" | python -m json.tool
