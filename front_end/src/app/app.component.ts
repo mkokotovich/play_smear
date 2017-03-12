103f9875cc9fdeb8b053d82915bda89fd030ad83
Matthew Kokotovich <mkokotovich@gmail.com>
2017-03-12 01:26:34 -0600

    Try to fix google analytics

--- front_end/src/app/app.component.ts
+++ front_end/src/app/app.component.ts
@@ -17,10 +17,8 @@ export class AppComponent {
 
     // This logic helps track page views using google analytics
     constructor(router:Router) {
-        // Add the Google Analytics javascript if running in production mode
+        // Send pageview to Google Analytics if running in production mode
         if (environment.production) {
-            document.write('<script type="text/javascript">(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,"script","https://www.google-analytics.com/analytics.js","ga"); ga("create", "UA-93535524-1", "auto"); </script>');
-
             // Using Rx's built in `distinctUntilChanged ` feature to handle url change
             router.events.distinctUntilChanged((previous: any, current: any) => {
                 // Subscribe to any `NavigationEnd` events where the url has changed
