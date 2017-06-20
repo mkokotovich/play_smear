be9f4b556654553e409de66675bdab089701454e
Matthew Kokotovich <mkokotovich@gmail.com>
2017-06-19 21:26:08 -0500

    Add script for easy access to database

---
+++ play_smear_api/tools/open_db.py
@@ -0,0 +1,25 @@
+# Run with python -i open_db.py
+
+from pymongo import MongoClient
+import os
+
+def initialize_mongo_db():
+    mongo_client = None
+    mongo_db = None
+    mongo_default_hostname = "localhost"
+    mongo_default_port = "27017"                                                                                                    
+    mongo_default_database = "smear"
+
+    if "MONGODB_URI" in os.environ:
+        uri = os.environ["MONGODB_URI"]
+        mongo_client = MongoClient(uri)
+        mongo_db = uri.split('/')[-1]
+        print "Using {} for mongodb database server, database: {}".format(uri.split('@')[-1].split('/')[0], mongo_db)
+    else:
+        mongo_client = MongoClient("{}:{}".format(mongo_default_hostname, mongo_default_port))
+        mongo_db = mongo_default_database
+        "Using {}:{} for mongodb database server, database: {}".format(mongo_default_hostname, mongo_default_port, g_mongo_db)
+
+    return mongo_client[mongo_db]
+
+db = initialize_mongo_db()
