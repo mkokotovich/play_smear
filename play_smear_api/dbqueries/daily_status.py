71352dc3eda6d64438cd071687fd244d145596cd
Matthew Kokotovich <mkokotovich@gmail.com>
2017-06-21 09:34:18 -0500

    Improvements to daily_status

--- play_smear_api/dbqueries/daily_status.py
+++ play_smear_api/dbqueries/daily_status.py
@@ -1,5 +1,7 @@
 from stat_gatherer import StatGatherer
 from datetime import datetime, timedelta
+from dateutil import tz
+import sys
 
 class DailyStatus():
     def __init__(self):
@@ -8,13 +10,21 @@ class DailyStatus():
         self.game_list = None
         self.bid_list = None
         self.player_list = None
+        self.local_time = None
 
 
-    def load_stats_since_yesterday(self):
+    def load_stats_since_previous_date(self, number_of_days):
         # Assume we are running some time after 4am CDT (which is 9am UTC)
         # "Yesterday" should go form 9am UTC on previous day to 9am UTC today
-        yesterday_start = datetime.utcnow() - timedelta(days=1)
+        yesterday_start = datetime.utcnow() - timedelta(days=number_of_days)
         yesterday_start = yesterday_start.replace(hour = 9, minute = 0, second = 0, microsecond = 0)
+
+        # Save the start time in local time zone
+        from_zone = tz.tzutc()
+        to_zone = tz.tzlocal()
+        yesterday_with_tz = yesterday_start.replace(tzinfo=from_zone)
+        self.local_time = yesterday_with_tz.astimezone(to_zone)
+
         self.game_list = list(self.stat.db.games.find({"date_added" : { "$gte" : yesterday_start }}))
         self.bid_list = list(self.stat.db.bids.find({"date_added" : { "$gte" : yesterday_start }}))
         self.player_list = list(self.stat.db.players.find())
@@ -28,7 +38,7 @@ class DailyStatus():
 
     
     def print_game_stats(self):
-        print "There have been {} games played since yesterday:".format(len(self.game_list))
+        print "There have been {} games played since {}:".format(len(self.game_list), self.local_time)
         for game in self.game_list:
             print "  Game {}:".format(game["_id"])
             print "    Players: {}".format(", ".join(self.find_player_names_from_ids(game["players"])))
@@ -39,8 +49,14 @@ class DailyStatus():
 
 def main():
 
+    number_of_days = 1
+    if len(sys.argv) > 1:
+        try:
+            number_of_days = int(sys.argv[1])
+        except ValueError:
+            print "Invalide number of days: " + sys.argv[1]
     daily_status = DailyStatus()
-    daily_status.load_stats_since_yesterday()
+    daily_status.load_stats_since_previous_date(number_of_days)
     daily_status.print_game_stats()
 
 
