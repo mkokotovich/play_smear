db6d9a5af462fc3519a0c75f6738d695d959c809
Matthew Kokotovich <mkokotovich@gmail.com>
2023-09-18 20:39:53 -0500

    fix: extend jwt

--- backend/api/settings/common.py
+++ backend/api/settings/common.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     "JWT_AUTH_HEADER_PREFIX": "Bearer",
-    "JWT_EXPIRATION_DELTA": timedelta(hours=24),
+    "JWT_EXPIRATION_DELTA": timedelta(days=31),
     "JWT_RESPONSE_PAYLOAD_HANDLER": "api.jwt.jwt_response_payload_handler",
 }
 
