45dc7698c0f08f84bf4d098b74a2089c85595cca
Matthew Kokotovich <mkokotovich@gmail.com>
2024-01-30 11:55:29 -0600

    adds XYChart to graph the score

--- ui/src/ScoreGraph.js
+++ ui/src/ScoreGraph.js
@@ -1,18 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Radio, Button, Modal } from 'antd';
 import axios from 'axios';
+import {
+  AnimatedAxis, // any of these can be non-animated equivalents
+  AnimatedGrid,
+  AnimatedLineSeries,
+  DataContext,
+  XYChart,
+} from '@visx/xychart';
+import { LegendOrdinal } from "@visx/legend";
+import { scaleOrdinal } from '@visx/scale';
 
 import getErrorString from './utils';
 
-const RadioButton = Radio.Button;
-const RadioGroup = Radio.Group;
+const accessors = {
+  xAccessor: (d) => d.x,
+  yAccessor: (d) => d.y,
+};
+
+const range = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
 
 function ScoreGraphVis(props) {
+  const {scoreData} = props;
+
+  const scoreDelta = scoreData.maxScore - scoreData.minScore;
+  const numTicks = scoreDelta > 10 ? scoreDelta/2 : scoreDelta;
+  const values = range(scoreData.minScore, scoreData.maxScore, 1)
+
+  function getDataForContestant(contestantName) {
+    const scores = scoreData.contestantData[contestantName].scores;
+    return scores.map((score, index) => ({x: index, y: score}));
+  }
+
+  function getColorForContestant(contestantName) {
+    return scoreData.contestantData[contestantName].color;
+  }
+
+  const ordinalColorScale = scaleOrdinal({
+    domain: scoreData.contestants,
+    range: scoreData.contestants.map((contestantName, index) => (getColorForContestant(contestantName))),
+  });
+
+  const ChartLegend = () => {
+    return (
+      <LegendOrdinal
+        direction="row"
+        itemMargin="8px 8px 8px 0"
+        scale={ordinalColorScale}
+        labelFormat={(label) => label.replace("-", " ")}
+        legendLabelProps={{ color: "black" }}
+        shape="box"
+        style={{
+          backgroundColor: "white",
+          marginTop: -24,
+          paddingLeft: 0,
+          color: 'black',
+          display: 'flex', // required in addition to `direction` if overriding styles
+        }}
+      />
+    );
+  };
+
+  const lineSeries = scoreData && scoreData.contestants.map((contestantName, index) => (
+    <AnimatedLineSeries key={index} dataKey={contestantName} colorAccessor={getColorForContestant} data={getDataForContestant(contestantName)} {...accessors} />
+  ));
+
   return (
     <>
-      Hi
+      <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+        <AnimatedAxis label="Score" orientation="left" tickValues={values}/>
+        <AnimatedAxis orientation="right" tickValues={values}/>
+        <AnimatedGrid columns={false} numTicks={numTicks} />
+        {lineSeries}
+      </XYChart>
+      <ChartLegend />
     </>
   );
 }
@@ -31,7 +95,17 @@ function ScoreGraph(props) {
     setLoading(true);
     axios.get(`/api/smear/v1/games/${game.id}`).then((response) => {
       setLoading(false);
-      setScoreData(response.data);
+      //setScoreData(response.data);
+      const tempScoreData = {
+        contestants: ["Team 1", "Team 2"],
+        contestantData: {
+          "Team 1": {color: "red", scores: [0, 2, 2, 5, 6, 7, 7]},
+          "Team 2": {color: "blue", scores: [0, -2, 2, 3, 6, 9, 11]},
+        },
+        maxScore: 11,
+        minScore: -2,
+      }
+      setScoreData(tempScoreData);
     }).catch((error) => {
       console.log(error);
       setLoading(false);
