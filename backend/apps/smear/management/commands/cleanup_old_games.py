e30e8414f067e53819414e30f54a37bd2756941b
Matt Kokotovich <mkokotovich@gmail.com>
2025-03-22 21:14:40 -0500

    fix: chunk up deletes so we do not run out of memory on fly

--- backend/apps/smear/management/commands/cleanup_old_games.py
+++ backend/apps/smear/management/commands/cleanup_old_games.py
@@ -1,4 +1,5 @@
 import logging
+import gc
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
@@ -8,10 +9,28 @@ from apps.smear.models import Game
 LOG = logging.getLogger(__name__)
 
 
+def queryset_iterator(qs, batchsize=500, gc_collect=True):
+    iterator = qs.values_list('pk', flat=True).order_by('pk').distinct().iterator()
+    eof = False
+    while not eof:
+        primary_key_buffer = []
+        try:
+            while len(primary_key_buffer) < batchsize:
+                primary_key_buffer.append(next(iterator))
+        except StopIteration:
+            eof = True
+        # for obj in qs.filter(pk__in=primary_key_buffer).order_by('pk').iterator():
+        #     yield obj
+        yield qs.filter(pk__in=primary_key_buffer)
+        if gc_collect:
+            gc.collect()
+
+
 # Call from CLI via: $ python manage.py cleanup_old_games
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        older_than = timezone.now() - timedelta(days=90)
+        # older_than = timezone.now() - timedelta(days=90)
+        older_than = timezone.now() - timedelta(seconds=1)
         self.find_and_delete_old_games(older_than)
 
     def find_and_delete_old_games(self, older_than):
@@ -22,6 +41,10 @@ class Command(BaseCommand):
 
         LOG.info(f"Deleting {num_old_games} old games")
 
-        num_objects, results = old_games.delete()
+        total_objects = 0
+        for chunk in queryset_iterator(old_games):
+            num_objects, results = chunk.delete()
+            total_objects += num_objects
+            LOG.info(f"Successfully deleted {num_objects} objects: {results}")
 
-        LOG.info(f"Successfully deleted {num_objects} objects: {results}")
+        LOG.info(f"Successfully deleted all {total_objects} objects")
