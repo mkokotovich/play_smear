066a0ac9f81df72be16f2983a785be85c282b1b9
Matt Kokotovich <mkokotovich@gmail.com>
2025-03-28 22:08:14 -0500

    upgrade react to 19

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,3 +1,4 @@
+import { createRoot } from "react-dom/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router }  from "react-router-dom";
@@ -5,10 +6,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
+root.render(<Router>
+  <App />
+</Router>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
