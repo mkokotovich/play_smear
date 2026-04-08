59bd60c84d5694a525e61d00738e360cf69c984d
Matt Kokotovich <mkokotovich@gmail.com>
2026-04-07 22:32:58 -0500

    switch email provider to resend

--- backend/apps/user/email.py
+++ backend/apps/user/email.py
@@ -1,16 +1,97 @@
 import logging
 import os
+from urllib.parse import quote
 
 import requests
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import resend
+
 
 LOG = logging.getLogger(__name__)
+resend.api_key = os.environ.get("RESEND_API_KEY", "default")
 
 
 def send_password_reset_email(to, reset_token):
     LOG.info(f"Sending password reset email to: {to}")
-    return send_password_reset_email_mailgun(to, reset_token)
+    return send_password_reset_email_resend(to, reset_token)
+
+
+def send_password_reset_email_resend(to, reset_token):
+    encoded_email = quote(to)
+    url = f"https://playsmear.com/#/reset?email={encoded_email}&token={reset_token}"
+    html_content = f"""
+<html>
+<head></head>
+<body>
+<h2>
+Play Smear Password Reset
+</h2>
+<p>
+<a href="{url}">Click Here</a> to reset your password, or copy and paste {url} into your browser
+</p>
+<br/><br/>
+</body>
+</html>
+    """
+
+    params: resend.Emails.SendParams = {
+        "from": "Playsmear Support <support@playsmear.com>",
+        "to": [to],
+        "subject": "Password Reset for playsmear.com",
+        "html": html_content,
+    }
+
+    try:
+        email = resend.Emails.send(params)
+        LOG.info(f"successfully sent password reset: {email}")
+    except Exception as e:
+        LOG.exception(f"Unable to send password reset email: {email}")
+        raise
+    return True
+
+
+def send_password_reset_email_brevo(to, reset_token):
+    brevo_url = "https://api.brevo.com/v3/smtp/email"
+    url = f"https://playsmear.com/#/reset?email={to}&token={reset_token}"
+    html_content = f"""
+<html>
+<head></head>
+<body>
+<h2>
+Play Smear Password Reset
+</h2>
+<p>
+<a href="{url}">Click Here</a> to reset your password, or copy and paste {url} into your browser
+</p>
+<br/><br/>
+</body>
+</html>
+    """
+
+    headers = {
+        "accept": "application/json",
+        "api-key": os.getenv("BREVO_API_KEY"),
+        "content-type": "application/json"
+    }
+
+    payload = {
+        "sender": {
+            "name": "Playsmear Support",
+            "email": "support@playsmear.com"
+        },
+        "to": [{"email": to}],
+        "subject": "Password Reset for playsmear.com",
+        "htmlContent": html_content,
+    }
+
+    try:
+        response = requests.post(brevo_url, headers=headers, json=payload)
+        response.raise_for_status()
+    except Exception as ex:
+        body = getattr(ex, "body", "unknown error")
+        LOG.exception(f"Unable to send password reset email: {body}")
+        raise
+
+    return True
 
 
 def send_password_reset_email_mailgun(to, reset_token):
@@ -53,44 +134,3 @@ Copy and paste {url} into your browser to reset your password
         raise
 
     return True
-
-
-def send_password_reset_email_sendgrid(to, reset_token):
-
-    url = f"https://playsmear.com/#/reset?email={to}&token={reset_token}"
-
-    message = Mail(
-        from_email="support@playsmear.com",
-        to_emails=to,
-        subject="Password Reset for playsmear.com",
-        html_content=f"""
-<h2>
-Play Smear Password Reset
-</h2>
-<p>
-<a href="{url}">Click Here</a> to reset your password, or copy and paste {url} into your browser
-</p>
-
-<p>
-<a href="https://playsmear.com">Unsubscribe</a>
-</p>
-<br/><br/>
-        """,
-    )
-
-    try:
-        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
-        response = sg.send(message)
-
-    except Exception as ex:
-        body = getattr(ex, "body", "unknown error")
-        LOG.exception(f"Unable to send password reset email: {body}")
-        raise
-
-    success_codes = [200, 202]
-    if response.status_code not in success_codes:
-        # TODO: Add logging
-        LOG.error(f"Unable to send password reset email. Status code: {response.status_code}, body: {response.body}")
-        raise Exception("Unable to send password reset email")
-
-    return response.status_code in success_codes
